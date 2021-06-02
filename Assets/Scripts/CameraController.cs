8bc963a1f914fa020f83d67c206e5e4948ff21e2
WhiteKaj <85242204+WhiteKaj@users.noreply.github.com>
2021-06-02 16:42:13 +0100

    Initial Repository Setup

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float cameraTurnSpeed = 1;
+    public float cameraRotationDamping = 0;
+    public float minYRot = -50, maxYRot = 45;
+    float mouseX, mouseY;
+    Camera playerCamera;
+    bool rotationInputAllowed = true;
+
+    void Start()
+    {
+        playerCamera = GetComponentInChildren<Camera>();
+
+        mouseX = this.gameObject.transform.eulerAngles.y;
+        mouseY = playerCamera.transform.eulerAngles.x;
+    }
+
+    void Update()
+    {
+        HandleRotation();
+    }
+
+    void HandleRotation()
+    {
+        if (!rotationInputAllowed)
+            return;
+
+        GameObject player = this.gameObject;
+
+        mouseX += Input.GetAxis("Mouse X") * cameraTurnSpeed;
+
+        mouseY -= Input.GetAxis("Mouse Y") * cameraTurnSpeed;
+        mouseY = Mathf.Clamp(mouseY, -maxYRot, -minYRot);
+
+        player.transform.rotation = Quaternion.Slerp(player.transform.rotation, Quaternion.Euler(0, mouseX, 0), cameraRotationDamping);
+        playerCamera.transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(mouseY, mouseX, 0), cameraRotationDamping);
+
+    }
+
+    Vector2 RotationVector()
+    {
+        return new Vector3(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+    }
+}
---
+++ Assets/Scripts/PlayerMovementController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementController : MonoBehaviour
+{
+    public float speed = 10;
+    CharacterController playerController;
+    Camera playerCamera;
+    bool movementInputAllowed = true;
+    void Start()
+    {
+        playerController = GetComponent<CharacterController>();
+        playerCamera = GetComponentInChildren<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleMovement();        
+    }
+    void HandleMovement()
+    {
+    
+        if(!movementInputAllowed)
+            return;
+        playerController.SimpleMove(Quaternion.LookRotation(playerCamera.transform.forward) * MovementInput() * speed * Time.deltaTime);
+    }
+
+    Vector3 MovementInput()
+    {
+        return new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
+    }
+}
